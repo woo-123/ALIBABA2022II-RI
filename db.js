da8a534f17ef4aaca8c1f8c7141bb35c9636dee2
Diego Chiroque <diego-al-18@hotmail.com>
2022-09-29 16:11:07 -0500

    commitDatos1.1

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user:"alphapisbeta43326",
-    host:"postgressdemo.postgres.database.azure.com",
-    database:"postgres",
-    password:"Admin1!!",
+    user:"rmubnlxpxrerwx",
+    host:"ec2-44-206-214-233.compute-1.amazonaws.com",
+    database:"d11qd9br5g15sk",
+    password:"0b10ac228650798d05ba18955fd9d486e5e2503134b383e39495f091fa709e4f",
     port: 5432,
     ssl:{
-        rejectUnauthorized:true,
+        rejectUnauthorized:false,
     }
 })
 module.exports = pool; 
